356b8a76148aa274ec135896cf1fc43fa51e43b0
ThieryNisengwe <thierynisengwe@gmail.com>
2022-08-01 01:21:55 -0700

    hello_world

---
+++ fundamentals/introduction/hello_world.py
@@ -0,0 +1,51 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print("Hello", name)	# with a comma
+print( "Hello " + name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print("Hello", name) 	# with a comma
+# print( "Hello"+ name) 	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}".format(fave_food1,fave_food2)) # with .format()
+print( f"I love to eat {fave_food1} and {fave_food2}" ) # with an f string
+
+fave_shoes1 = "Nike"
+fave_shoes2 = "Addias"
+
+print("I love to wear {} and {} are my second favorite".format(fave_shoes1,fave_shoes2))
+
+t = "Thiery"
+
+print("Hello", t)
+
+print("Hello " + t)
+
+y = 34
+
+print("Hello", y)
+
+print("Hello " + str(y) )
+
+fave_food1 = "Teryaki"
+fave_food2 = "Hamburger"
+
+print("I love to eat {} and {} ".format(fave_food1,fave_food2))
+
+print(f"I love to eat {fave_food1} and {fave_food2}")
+
+s = '   hello '
+s = s[3:8]
+
+print(s)
+
+w = 'these are random words'
+
+w = w[1:10]
+
+print(w)
+
